c53d58ba3c51e7697edc7bf0fccd10b06d51e08d
amanbahata <bahataaman@gmail.com>
2019-01-06 19:40:28 +0000

    Addition of a start button

--- app/src/main/java/com/aman1/rxjavatest/MVP/MainActivity.java
+++ app/src/main/java/com/aman1/rxjavatest/MVP/MainActivity.java
@@ -11,14 +11,17 @@ import com.aman1.rxjavatest.data.ApiClient;
 import com.aman1.rxjavatest.R;
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity implements Contract.View {
+import io.reactivex.internal.operators.observable.BlockingObservableNext;
+
+public class MainActivity extends AppCompatActivity implements Contract.View, View.OnClickListener{
 
     private static final String TAG = "MainActivity";
 
     private Contract.Presenter presenter;
-    private Button button;
+    private Button next_button;
     private ImageView imageView;
     private TextView textView;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +30,12 @@ public class MainActivity extends AppCompatActivity implements Contract.View {
 
         presenter = new Presenter(this, new ApiClient());
 
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (presenter != null) {
-                    presenter.getCats();
-                }
-            }
-        });
+        startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(this);
+
+        next_button = findViewById(R.id.next_button);
+        next_button.setOnClickListener(this);
+
         imageView = findViewById(R.id.imageView);
         textView = findViewById(R.id.textView);
     }
@@ -47,6 +47,17 @@ public class MainActivity extends AppCompatActivity implements Contract.View {
             if (textView != null){
                 textView.setText(url);
             }
+        }
+        if (next_button != null && next_button.getVisibility() == View.INVISIBLE){
+            next_button.setVisibility(View.VISIBLE);
+            startButton.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (presenter != null) {
+            presenter.getCats();
         }
     }
 }
